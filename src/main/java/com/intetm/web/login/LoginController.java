8ded525a73d67969ada008e75a7ffba815b71116
Sivodedov Dmitry <intetm@gmail.com>
2015-11-27 22:19:26 +0300

    logback

--- src/main/java/com/intetm/web/login/LoginController.java
+++ src/main/java/com/intetm/web/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.intetm.web.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     private static final String HELLO_VIEW = "hello";
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(Model model) {
+        logger.debug("hello page");
         model.addAttribute("subject", "world");
         return HELLO_VIEW;
     }
